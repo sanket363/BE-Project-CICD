c2685ef879d212354d94ed337f819595b619d2bd
sanket363 <sanketbhalke363@gmail.com>
2024-01-17 14:33:29 +0530

    commit

--- terraform-scripts/provider.tf
+++
@@ -1,3 +0,0 @@
-provider "aws" {
-  region = "us-west-1" # Change to your desired AWS region
-}
---
+++ terraform-scripts/vars.tf
@@ -0,0 +1,6 @@
+variable "access_key" {
+  default = "<YOUR-AWS-ACCESS-KEY>"
+}
+variable "secret_key" {
+    default = "<YOUR-AWS-SECRET-KEY>"
+}
