048e766d9a8dc8631f89471c9f31174adbc759fd
sanket363 <98816965+sanket363@users.noreply.github.com>
2024-01-31 17:38:13 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket = "sanket-terraform-backend12"
+   bucket = "terraform-backend-bucket-sanket"
    key    = "eks/terraform.tfstate"
    region = "us-east-1"
  }
